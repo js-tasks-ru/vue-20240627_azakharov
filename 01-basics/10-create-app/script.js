dbc9824ea2659c56b08c28798d327da0a923c48a
aleksey88zakh <zakh.alexey@gmail.com>
2024-08-01 10:29:44 +0600

    Task 2 / Create app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,19 @@
 import { defineComponent, createApp } from 'vue/dist/vue.esm-bundler.js'
+
+const App = defineComponent({
+  name: 'App',
+
+  setup() {
+    const date = new Intl.DateTimeFormat('en', { dateStyle: 'long' }).format(new Date())
+
+    const title = 'Сегодня ' + date
+
+    return {
+      title,
+    }
+  },
+
+  template: `<div>{{ title }}</div>`,
+})
+
+createApp(App).mount('#app')
